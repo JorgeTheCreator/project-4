2da18dacaf2bf9bce6b4f5348ea6f250a64a2e74
JorgeTheCreator <JrgPeguero@yahoo.com>
2018-12-06 02:22:17 -0500

    add config file

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb://jorge123:jorge123@cluster0-shard-00-00-vgeeq.mongodb.net:27017,cluster0-shard-00-01-vgeeq.mongodb.net:27017,cluster0-shard-00-02-vgeeq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'secret'
+};
